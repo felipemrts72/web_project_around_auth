d8766c74c7af7f31d947e2b5d94072b50d6bd006
FelipeMartins <felipemrts@icloud.com>
2025-07-20 21:04:38 -0400

    adds header button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,6 @@
+const isLogged = false;
+const email = 'email@mail.com';
+
 function Header() {
   return (
     <header className="header">
@@ -6,6 +9,20 @@ function Header() {
           alt="Logo do site, ao logo dos estados unidos"
           className="header__logo-item"
         />
+        <div className="header__button-wrapper">
+          <button className="header__button">
+            {isLogged ? email : 'Entrar'}
+          </button>
+          <button
+            className={
+              isLogged
+                ? 'header__button header__button_type_active'
+                : 'header__button'
+            }
+          >
+            {isLogged ? 'Sair' : ''}
+          </button>
+        </div>
       </div>
     </header>
   );
