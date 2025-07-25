fc70f73ec5cb00eefd30e488daf8116ebdf705e1
FelipeMartins <felipemrts@icloud.com>
2025-07-25 15:26:59 -0400

    fix DRY

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,46 +1,37 @@
 export const BASE_URL = 'https://se-register-api.en.tripleten-services.com/v1';
 
-export const register = async ({ email, password }) => {
-  const res = await fetch(`${BASE_URL}/signup`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ email, password }),
-  });
-
-  if (!res.ok) {
-    throw new Error('Erro no registro');
-  }
-
-  const data = await res.json();
-  return data;
-};
-export const authorize = async ({ email, password }) => {
-  const res = await fetch(`${BASE_URL}/signin`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ email, password }),
-  });
-
-  const data = await res.json();
+async function getData(url, options) {
+  const res = await fetch(url, options);
 
   if (!res.ok) {
     throw res.status;
   }
 
-  return data;
+  return res.json();
+}
+
+export const register = ({ email, password }) => {
+  return getData(`${BASE_URL}/signup`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ email, password }),
+  });
 };
 
-export const checkToken = async (token) => {
-  return fetch(`${BASE_URL}/users/me`, {
+export const authorize = ({ email, password }) => {
+  return getData(`${BASE_URL}/signin`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ email, password }),
+  });
+};
+
+export const checkToken = (token) => {
+  return getData(`${BASE_URL}/users/me`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${token}`,
     },
-  }).then((res) => {
-    if (!res.ok) {
-      return Promise.reject(res.status);
-    }
-    return res.json();
   });
 };
